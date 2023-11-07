f34db97fb8feb9a3c3e09a52964f3234bfea64e3
Lê Văn Quốc <94890892+kuroko286@users.noreply.github.com>
2023-11-07 17:30:24 +0700

    init backend

---
+++ src/main/java/com/magicpost/MagicpostApplication.java
@@ -0,0 +1,13 @@
+package com.magicpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicpost/MagicpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
